88349fb1bc99ef57c84053875e9ddf977fc07efa
Sagar Karandikar <sagark@eecs.berkeley.edu>
2019-03-28 05:18:09 +0000

    match firechip

--- build.sh
+++ build.sh
@@ -14,13 +14,13 @@ check_version() {
     )
 }
 
-check_version automake 1.14 "OpenOCD build"
-check_version autoconf 2.64 "OpenOCD build"
-build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --enable-jtag_vpi --disable-werror
+#check_version automake 1.14 "OpenOCD build"
+#check_version autoconf 2.64 "OpenOCD build"
+#build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --enable-jtag_vpi --disable-werror
 
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
-build_project riscv-gnu-toolchain --prefix=$RISCV
+build_project riscv-gnu-toolchain --prefix=$RISCV --with-arch=rv64imafd
 CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv64-unknown-elf
 build_project riscv-tests --prefix=$RISCV/riscv64-unknown-elf
 
